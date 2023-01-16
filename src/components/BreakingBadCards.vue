56b5c40d8e8b2e56421b27fd6268dfa5232cd662
LucasPerezDev <LucasPerezDev@gmail.com>
2023-01-16 15:44:45 -0500

    Learned to use Axios to fetch data from the API. Also learned that the Composition API and script setup automatically make top-level functions async/await by default when you specify await. I combined this with the Suspense feature of Vue in order to render a #default option which would load our BreakingBadCard component and a #fallback that renders Loading... in case something wasn't working within our #default option

---
+++ src/components/BreakingBadCards.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import axios from "axios"
+//A note about not needing async syntax for awaiting the response. 
+//The composition API & script setup, they are automatically async + await. 
+  const response = await axios.get("https://www.breakingbadapi.com/api/character/random")
+
+  console.log(response)
+</script>
+
+<template>
+  <div>
+    <h1>Breaking Bad Cards</h1>
+  </div>
+</template>
