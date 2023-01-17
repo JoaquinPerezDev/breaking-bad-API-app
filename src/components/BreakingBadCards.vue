230dfa825bceb29512c638c4071d8cbd734f11a4
LucasPerezDev <LucasPerezDev@gmail.com>
2023-01-17 14:50:28 -0500

    Learned to use the API's limit & offset feature to speficially fetch 8 characters at a time, as opposed to all characters at once

--- src/components/BreakingBadCards.vue
+++ src/components/BreakingBadCards.vue
@@ -2,13 +2,13 @@
   import axios from "axios"
 //A note about not needing async syntax for awaiting the response. 
 //The composition API & script setup, they are automatically async + await. 
-  const response = await axios.get("https://www.breakingbadapi.com/api/character/random")
+  const response = await axios.get("https://www.breakingbadapi.com/api/characters?limit=8")
 
-  console.log(response)
 </script>
 
 <template>
   <div>
     <h1>Breaking Bad Cards</h1>
+    <h1>{{response}}</h1>
   </div>
 </template>
